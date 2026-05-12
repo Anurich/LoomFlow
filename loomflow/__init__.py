e158ed60eed9bdda6a4e2194769d7a1d362fcea8
Anurich <nautiyalanupam98@gmail.com>
2026-05-12 16:25:30 +0530

    Release v0.9.38

--- loomflow/__init__.py
+++ loomflow/__init__.py
@@ -106,7 +106,7 @@ from .security import (
 from .tools import Tool, tool
 from .workflow import END, START, Workflow, WorkflowResult, step
 
-__version__ = "0.9.37"
+__version__ = "0.9.38"
 
 __all__ = [
     "__version__",
